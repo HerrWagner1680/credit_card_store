3985a281a64183abe84ea69a9bccedeb55b4f4a6
Sidney Wagner <herrwagner1680@gmail.com>
2014-11-23 13:45:02 -0500

    added card dropdown

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -6,6 +6,9 @@ class OrdersController < ApplicationController
   end
 
   def new
+    current_user # returns @current_user
+    @user = session[:user_id]
+    @cards = Card.where(user_id: session[:user_id])
   end
 
   def show
